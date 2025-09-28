75985f04b364eeabfe81252ed3d3d57b5a22b62a
08183080 <08183080@cumt.edu.cn>
2025-09-28 14:54:32 +0800

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ if [ -z $KEY ]; then
     exit 0
 fi
 
-# Update the openAI key
-for x in ChatAFL ChatAFL-CL1 ChatAFL-CL2;
+
+for x in ChatAFL xpgfuzz;
 do
   sed -i "s/#define OPENAI_TOKEN \".*\"/#define OPENAI_TOKEN \"$KEY\"/" $x/chat-llm.h
 done
 
-# Copy the different versions of ChatAFL to the benchmark directories
+
 for subject in ./benchmark/subjects/*/*; do
   rm -r $subject/aflnet 2>&1 >/dev/null
   cp -r aflnet $subject/aflnet
@@ -19,11 +19,9 @@ for subject in ./benchmark/subjects/*/*; do
   rm -r $subject/chatafl 2>&1 >/dev/null
   cp -r ChatAFL $subject/chatafl
   
-  rm -r $subject/chatafl-cl1 2>&1 >/dev/null
-  cp -r ChatAFL-CL1 $subject/chatafl-cl1
+  rm -r $subject/xpgfuzz 2>&1 >/dev/null
+  cp -r ChatAFL-CL1 $subject/xpgfuzz
   
-  rm -r $subject/chatafl-cl2 2>&1 >/dev/null
-  cp -r ChatAFL-CL2 $subject/chatafl-cl2
 done;
 
 # Build the docker images
